46f30e920bc1130519b228deb5e469cdbb506bc3
ShirFeld <shirfeldman1103@gmail.com>
2023-02-15 17:09:00 +0200

    Tag arrangement in put page

--- src/Delete.php
+++ src/Delete.php
@@ -28,18 +28,15 @@ $controller = new ArticleController($gateway);
 </style>
 </head>
 <body>
-
 <nav class="navbar navbar-inverse">
   <div class="container-fluid">
     <ul class="nav navbar-nav">
-
       <li><a href="http://localhost/inManage/api/src/Articles.php">Articles</a></li>
       <li><a href="http://localhost/inManage/api/src/Put.php">Edit article</a></li>
       <li><a href="http://localhost/inManage/api/src/Upload.php">Upload article</a></li>
     </ul>
   </div>
 </nav>
-
 <div class="row">
         <div class="col-md-12">
             <h1 class=" text-center" style="  color: lightblue;
@@ -52,21 +49,14 @@ $controller = new ArticleController($gateway);
         <div class="panel panel-default">
             <div class="panel-body">
             <form action="" method="GET">
-                <br>
-                <!-- <div class="input-group"> -->
-                    <!-- <span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span> -->
-                    <div style="text-align: center; color:#3d9aa7; margin-bottom:10px">
-                    <h4 ><strong>Enter an article id to delete </strong></h4>
-                    </div>
-
-                    
-                    <input id="articleId" type="number" class="form-control" name="articleId" placeholder="Article Id">
-                <!-- </div> -->
-                <br>
-                <button type="submit" class="btn-block btn-primary" name="submit"> <h5> <b> Send </b></h5></button>
-                </form>
-  
-                <br>
+              <div style="text-align: center; color:#3d9aa7; margin-bottom:10px">
+                <h4 ><strong>Enter an article id to delete </strong></h4>
+              </div>
+              <input id="articleId" type="number" class="form-control" name="articleId" placeholder="Article Id">
+              <br>
+              <button type="submit" class="btn-block btn-primary" name="submit"> <h5> <b> Send </b></h5></button>
+            </form>
+            <br>
             </div>
             </div>
 
--- src/Put.php
+++ src/Put.php
@@ -24,62 +24,57 @@ $controller = new ArticleController($gateway);
 </head>
 <body>
 
-<nav class="navbar navbar-inverse">
-  <div class="container-fluid">
-    <ul class="nav navbar-nav">
-    <li><a href="http://localhost/inManage/api/src/Articles.php">Articles</a></li>
-    <li><a href="http://localhost/inManage/api/src/Upload.php">Upload article</a></li>
-      <li><a href="http://localhost/inManage/api/src/Delete.php">Delete article</a></li>
-    </ul>
+  <nav class="navbar navbar-inverse">
+    <div class="container-fluid">
+      <ul class="nav navbar-nav">
+        <li><a href="http://localhost/inManage/api/src/Articles.php">Articles</a></li>
+        <li><a href="http://localhost/inManage/api/src/Upload.php">Upload article</a></li>
+        <li><a href="http://localhost/inManage/api/src/Delete.php">Delete article</a></li>
+      </ul>
+    </div>
+  </nav>
+
+
+  <div class="container">
+      <div class="row">
+          <div class="col-md-12">
+              <h1 class=" text-center" style="  color: lightblue;
+                          text-shadow: 2px 2px 4px black; font-size: 60px;" > <b> Update article </b></h1>
+          </div>
+      </div>
+      <br> <br>
+      <div class="row">
+          <div class="col-md-offset-3 col-md-6">
+          <div class="panel panel-default">
+              <div class="panel-body">
+                <form action="" method="GET">              
+                  <div style="text-align: center; color:#3d9aa7; margin-bottom:10px">
+                      <h4 ><strong>You can update your article here or in url </strong></h4>
+                  </div>
+                  <input id="id" type="number" class="form-control" name="id" placeholder="Article id"><br>
+                  <input id="article_name" type="text" class="form-control" name="article_name" placeholder="Article name"><br>
+                  <input id="length" type="number" class="form-control" name="length" placeholder="Minutes Long"><br>
+                  <input id="publish_date" type="datetime-local" class="form-control" name="publish_date" placeholder="Publish Date"><br>
+                  <label >Enter your content here</label><br>
+                  <textarea  style="margin-bottom :40px" rows = "18" cols = "70" name = "content"></textarea><br>
+                  <button type="submit" class="btn-block btn-primary" value="login" name="update"> <h5> <b> update </b></h5></button>
+                </form>
+              </div>
+          </div>
+        
+              <?php
+                if (isset($_GET['update'])){
+                  //HTML
+                  $controller->controllerUpdate("PATCH" ,$_GET['id'] ,$_GET['article_name'] , $_GET['length'] , $_GET['publish_date'], $_GET['content']);
+                }
+                elseif(isset($_GET['id'])){
+                  // by url   file.php?id=3
+                  $controller->controllerUpdate("PATCH" ,$_GET["id"] ,$_GET['article_name'] , $_GET['length'] , $_GET['publish_date'], $_GET['content']);
+    
+                }
+              ?>
+          </div>
+      </div>
   </div>
-</nav>
-
-
-<div class="container">
-    <div class="row">
-        <div class="col-md-12">
-            <h1 class=" text-center" style="  color: lightblue;
-                        text-shadow: 2px 2px 4px black; font-size: 60px;" > <b> Update article </b></h1>
-        </div>
-    </div>
-    <br> <br>
-    <div class="row">
-        <div class="col-md-offset-3 col-md-6">
-        <div class="panel panel-default">
-            <div class="panel-body">
-            <div style="color:green">
-             </div>
-              <form action="" method="GET">
-                <input id="id" type="number" class="form-control" name="id" placeholder="Article id">
-                <br>
-                <input id="article_name" type="text" class="form-control" name="article_name" placeholder="Article name">
-                <br>
-                <input id="length" type="number" class="form-control" name="length" placeholder="Minutes Long">
-                <br>
-                <input id="publish_date" type="datetime-local" class="form-control" name="publish_date" placeholder="Publish Date">
-                <br>
-                <label >Enter your content here</label><br>
-                <textarea  style="margin-bottom :40px" rows = "20" cols = "70" name = "content"></textarea>
-                <br>
-                <button type="submit" class="btn-block btn-primary" value="login" name="update"> <h5> <b> update </b></h5></button>
-              </form>
-               
-            </div>
-            </div>
-      
-            <?php
-              if (isset($_GET['update'])){
-                //HTML
-                $controller->controllerUpdate("PATCH" ,$_GET['id'] ,$_GET['article_name'] , $_GET['length'] , $_GET['publish_date'], $_GET['content']);
-              }
-              elseif(isset($_GET['id'])){
-                 // by url   file.php?id=3
-                $controller->controllerUpdate("PATCH" ,$_GET["id"] ,$_GET['article_name'] , $_GET['length'] , $_GET['publish_date'], $_GET['content']);
-  
-              }
-            ?>
-        </div>
-    </div>
-</div>
 </body>
 </html>
